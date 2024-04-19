67369ef93f915771be04e5f8282111f8fe415ec9
Chenwei Zhang <chenwz@amazon.com>
2024-04-19 16:04:52 -0700

    "add storage"

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
+// import { data } from './data/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
   auth,
-  data,
+  // data,
 });
